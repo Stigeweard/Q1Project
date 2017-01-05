59d372dbf8049a625e0d2e2eac5ba787bd0e8265
C S Harding <charliesharding@gmail.com>
2017-01-05 16:22:56 -0600

    favicon working

--- server.js
+++ server.js
@@ -2,12 +2,17 @@ const express = require('express');
 const app = express();
 const knex = require('./knex');
 const bodyParser = require('body-parser');
+const favicon = require('serve-favicon');
+const path = require('path');
+
 
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
 app.use(express.static('public'));
+app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
+
 const userRoutes = require('./routes/userRoutes');
 
 app.use(userRoutes)
